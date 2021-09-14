36fee9557d640e0c61b174194c7a946545d9f89a
javaravikiran <ravikiran.namana@gmail.com>
2021-09-14 21:17:49 +0530

    initialcommit

---
+++ src/test/java/com/example/Eureka/Server/App/EurekaServerAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.Eureka.Server.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+	
+
+}
